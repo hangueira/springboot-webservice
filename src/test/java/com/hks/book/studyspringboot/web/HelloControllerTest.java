7d72a41c25b8343a6a13d824c16f04fc627d1980
hankyungsu <hks713@naver.com>
2021-12-31 23:17:50 +0900

    HelloController 작성 및 테스트 코드 작성

---
+++ src/test/java/com/hks/book/studyspringboot/web/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.hks.book.studyspringboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc; //스프링 MVC 테스트 시작점으로 이 클래스를 통해 HTTP GET, POST API 테스트 할 수 있다.
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
